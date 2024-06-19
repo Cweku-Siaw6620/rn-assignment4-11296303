117aafab9b73e3d1e48af8c66862fa5611d469b0
Kelvin Ashong <kksashong@st.ug.edu.gh>
2024-06-19 23:23:19 +0000

    Continued with my homepage

--- Project_assignment/login.js
+++ Project_assignment/login.js
@@ -10,10 +10,8 @@ export default function Login(){
 
 
     const handleLogin = () => {
-        // Perform validation (for simplicity, check if fields are not empty)
         if (username && email) {
-          // Navigate to home screen (replace 'HomeScreen' with your actual home screen component)
-          navigation.navigate('HomeScreen');
+          navigation.navigate('HomeScreen',{username,email});
         } else {
           alert('Please enter username and email.');
         }
